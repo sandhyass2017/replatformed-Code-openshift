cf0f9881e7afa484f2c23f1956d526cd285aa963
sandhyass2017 <sandhya.sujathasoman@cognizant.com>
2018-04-23 16:47:14 +0530

    pom added

--- src/main/java/com/hellokoding/auth/WebApplication.java
+++ src/main/java/com/hellokoding/auth/WebApplication.java
@@ -1,13 +1,18 @@
 package com.hellokoding.auth;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
+@EnableAutoConfiguration
 @SpringBootApplication
-@ComponentScan("com.hellokoding.auth")
+@ComponentScan({"com.hellokoding.auth"})
+@PropertySources(value = { @PropertySource("classpath:/application.properties") })
 public class WebApplication extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
